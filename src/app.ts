a54964ef922b923c766e25e71e3f52ba6582bb3a
Ahmad Albarasy <ahmadbarasy@gmail.com>
2024-07-21 19:18:58 +0300

    added global error handler on the app level for unexpected errors

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import morgan from "morgan";
-import express, { Request, Response, Express } from "express";
+import express, { Request, Response, Express, NextFunction } from "express";
 import apiRouter from "./routes/apiRouter";
+import errorController from "./controllers/errorController";
 
 const app: Express = express();
 
@@ -14,5 +15,9 @@ app.route("*").all((req: Request, res: Response) =>
 		.status(404)
 		.json({ status: "fail", message: "Endpoint not implemented." }),
 );
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) =>
+	errorController(err, req, res, next),
+); // error handler in case any error happened during receiving and processing the request using the body parser.
 
 export default app;
